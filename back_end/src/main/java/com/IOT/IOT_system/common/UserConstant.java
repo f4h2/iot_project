e6c00d755f72e43e543ea8ad62fd91d4a3c89530
f4h2 <truonglocnguyenkhanh@gmail.com>
2024-01-09 10:56:43 +0700

    new

--- back_end/src/main/java/com/IOT/IOT_system/common/UserConstant.java
+++
@@ -1,9 +0,0 @@
-package com.IOT.IOT_system.common;
-
-public class UserConstant {
-
-
-    public static final String DEFAULT_ROLE = "ROLE_USER";
-    public static final String[] ADMIN_ACCESS = {"ROLE_ADMIN"};
-
-}
